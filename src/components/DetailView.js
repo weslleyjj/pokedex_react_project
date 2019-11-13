640dfcfcb539ec36a34d7e7fdb45ada7dec62ae6
J. Weslley <47761985+weslleyjj@users.noreply.github.com>
2019-11-13 11:16:05 -0300

    v1.3
    corrigido bug: Agora é reconhecido a mudança de estado ao clicar em determinador pokemon
    
    TODO:
    - Acabar com o loop infinito do render para poder implementar a div detalhes

--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -4,23 +4,35 @@ import "./styles/DetailView.css";
 import Detalhes from "./Detalhes";
 import Pokemon from "../Pokemon";
 
-const DetailView = ({pokemon}) => {
-    const {id, name, sprite, type, weight} = pokemon;    
-    
-    return (
-        <section className="detail-view">
-            <img src={sprite} className="sprite-image" alt="sprite" />
-            <div className="data-wrapper">
-                <h4 className="data-name">ID: {id} {name} </h4>
-                <p className="data-char"> Tipo: {type} </p>
-                <p className="data-char">Peso: {weight} kg</p>
-            </div>            
-            <button type="button" className="btn btn-success detalhes" data-toggle="collapse" data-target="#detalhes" 
-            onClick={ () => {
-                ReactDOM.render(<Detalhes num={id} />, document.getElementById("detalhes"));
-            } }  >Mais Detalhes </button>
-        </section>
-    );
-}
+const DetailView = ({ pokemon }) => {
+	const { id, name, sprite, type, weight } = pokemon;
+
+	return (
+		<section className="detail-view">
+			<img src={sprite} className="sprite-image" alt="sprite" />
+			<div className="data-wrapper">
+				<h4 className="data-name">
+					ID: {id} {name}{" "}
+				</h4>
+				<p className="data-char"> Tipo: {type} </p>
+				<p className="data-char">
+					Peso: {parseFloat((weight / 2.2046).toFixed(2))} kg
+				</p>
+			</div>
+			<button
+				type="button"
+				className="btn btn-success detalhes"
+				onClick={() => {
+					ReactDOM.render(
+						<Detalhes id={id} />,
+						document.getElementById("detalhes")
+					);
+				}}
+			>
+				Mais Detalhes{" "}
+			</button>
+		</section>
+	);
+};
 
 export default DetailView;
--- src/components/Detalhes.js
+++ src/components/Detalhes.js
@@ -1,18 +1,36 @@
 import React from "react";
-import Pokemon from "../Pokemon";
 
-const Detalhes = (props) => {
-    let pokemon;
-    fetch(`http://pokeapi.co/api/v2/pokemon/${props.num}/`);
-        
-    console.log(pokemon.name);
-    return (
-        <div className="container">
-            <div className="row">
-                <h1>Nome: {} </h1>
-            </div>
-        </div>
-    );
+class Detalhes extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			id: 0,
+			isLoading: true,
+			isLoaded: false,
+			pokemon: {}
+		};
+	}
+	carregarPokemon() {
+		fetch(`https://pokeapi.co/api/v2/pokemon/${this.props.id}`)
+			.then(response => response.json())
+			.then(data =>
+				this.setState({
+					id: this.props.id,
+					isLoading: false,
+					isLoaded: true,
+					pokemon: data
+				})
+			);
+	}
+
+	componentDidMount() {
+		this.carregarPokemon();
+	}
+	render() {
+		this.componentDidMount();
+		console.log(this.state.pokemon.name);
+		return <h1>Hello, {this.state.pokemon.name}</h1>;
+	}
 }
 
 export default Detalhes;
